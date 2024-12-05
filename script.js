a31e16cde97fd76ba16bcd3fcdb206721f1bc744
shafwannadzmee <nadzmeeshafwan32@gmail.com>
2024-12-05 13:49:10 +0800

    update login form

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ var TrandingSlider = new Swiper('.tranding-slider', {
     },
     allowTouchMove: false, // Disable all touch-based sliding/swiping
 });
+
+// Get modal elements
+const modal = document.getElementById('modal');
+const openModal = document.getElementById('openModal');
+const closeModal = document.getElementById('closeModal');
+
+// Open the modal
+openModal.addEventListener('click', () => {
+  modal.style.display = 'flex'; // Flex to center
+});
+
+// Close the modal
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// Close modal when clicking outside of it
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
